f6a2278da53977f66802da2c76ace465963bac11
suryamaris <smarispanensa@gmail.com>
2021-01-14 22:32:06 +0700

    Data awal

---
+++ rekap.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class rekap
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.kasMasuk = New System.Windows.Forms.Button()
+        Me.kembali = New System.Windows.Forms.Button()
+        Me.cetak = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 74)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 275)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cambria", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 28)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Rekapitulasi"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Myriad Arabic", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DarkCyan
+        Me.Label5.Location = New System.Drawing.Point(530, 1)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(258, 70)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "Klinik Dr. Indra"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(170, 365)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 46)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Kas Keluar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'kasMasuk
+        '
+        Me.kasMasuk.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kasMasuk.Location = New System.Drawing.Point(12, 365)
+        Me.kasMasuk.Name = "kasMasuk"
+        Me.kasMasuk.Size = New System.Drawing.Size(133, 46)
+        Me.kasMasuk.TabIndex = 25
+        Me.kasMasuk.Text = "Kas Masuk"
+        Me.kasMasuk.UseVisualStyleBackColor = True
+        '
+        'kembali
+        '
+        Me.kembali.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kembali.Location = New System.Drawing.Point(693, 365)
+        Me.kembali.Name = "kembali"
+        Me.kembali.Size = New System.Drawing.Size(95, 46)
+        Me.kembali.TabIndex = 27
+        Me.kembali.Text = "Kembali"
+        Me.kembali.UseVisualStyleBackColor = True
+        '
+        'cetak
+        '
+        Me.cetak.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cetak.Location = New System.Drawing.Point(582, 365)
+        Me.cetak.Name = "cetak"
+        Me.cetak.Size = New System.Drawing.Size(95, 46)
+        Me.cetak.TabIndex = 28
+        Me.cetak.Text = "Cetak"
+        Me.cetak.UseVisualStyleBackColor = True
+        '
+        'rekap
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cetak)
+        Me.Controls.Add(Me.kembali)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.kasMasuk)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "rekap"
+        Me.Text = "rekap"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents kasMasuk As Button
+    Friend WithEvents kembali As Button
+    Friend WithEvents cetak As Button
+End Class
---
+++ rekap.vb
@@ -0,0 +1,3 @@
+﻿Public Class rekap
+
+End Class
